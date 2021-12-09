acb5dfafa9d79df430dbab58f817f20d5b29ef39
manisha <manishakarus2142@gmail.com>
2021-12-09 12:28:28 +0530

    adding files
    adding files

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+});
